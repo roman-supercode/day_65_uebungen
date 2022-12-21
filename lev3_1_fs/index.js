9415e6febba647084cb861b8d8a5425cfabf3009
roman-supercode <roman.supercode@gmail.com>
2022-12-21 14:31:09 +0100

    lvl2 & 3 complete

--- lev3_1_fs/index.js
+++ lev3_1_fs/index.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 
+
 // 1. Erstelle einen Unterordner innerhalb deines Projektordners. In diesem Ordner soll die Datei angelegt werden.
 // fs.mkdir("./Unterordner", ((err) => {
 //     if (err) return console.log("ERROR", err);
@@ -8,17 +9,20 @@ import fs from "fs";
 
 
 // 2. Schreibe eine Funktion, die einen Parameter annimmt.
-const checking = (param) => {
-    const zeilenumbruch = `${param}\n`;
-    fs.writeFile("./Unterordner/test.txt", zeilenumbruch, { flag: 'a' }, ((err) => {
-        if (err) return console.log("ERROR:", err);
-        console.log("DATEI ERSTELLT!");
-    }));
+const checkingPromise = (param) => {
+    return new Promise((resolve, reject) => {
+        const zeilenumbruch = `${param}\n`;
+        fs.writeFile("./Unterordner/test.txt", zeilenumbruch, { flag: 'a' }, ((err) => {
+            if (err) return reject(err);
+            else resolve();
+        }));
+    });
 };
 
-checking("HALLO");
+checkingPromise("HALLO")
+    .then(() => console.log("DATEI ERSTELLT!"))
+    .catch(err => console.log("ERROR:", err));
 
-// fs.appendFileSync("data.txt", `\n ${ data.id } - ${ data.title }, (err) => { console.log(err); }`);
 
 
 
